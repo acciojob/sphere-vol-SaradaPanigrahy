1e30164be5375c13968055f2c65e041c7cfe69c9
SaradaPanigrahy <98700255+SaradaPanigrahy@users.noreply.github.com>
2025-09-29 21:10:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function volume_sphere() {
     //Write your code here
+	const radiusInput = document.getElementById("radius").value;
+    const radius = parseFloat(radiusInput);
+	const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+    // 4. Round to 4 decimal places
+    const roundedVolume = volume.toFixed(4);
+
+    // 5. Display result in the output field/span
+    document.getElementById("volume").textContent = roundedVolume;
   
 } 
 
